25b362ad65e1ab945cdeea813200c745d6dbaab6
«Elena <lingelena@hotmail.com>
2022-05-14 11:34:11 +0200

    Division of two numbers with or without % remainder

---
+++ work1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли второе число
+//кратным первому. Если число 2 не кратно числу 1, то
+//программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+
+int number1=new Random().Next(100,1000);
+int number2=new Random().Next(1,10);
+Console.WriteLine (number1);
+Console.WriteLine (number2);
+
+if (number1 % number2==0) Console.WriteLine("yes");
+else Console.WriteLine("no");
+
