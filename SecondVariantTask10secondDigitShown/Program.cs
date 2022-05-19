a720e11f318011c44903bbec774036e46f280253
«Elena <lingelena@hotmail.com>
2022-05-19 23:08:46 +0200

    Second variant of task 10 Second digit shown of three digit number

---
+++ SecondVariantTask10secondDigitShown/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на 
+//вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//using System;
+
+Console.Write("Enter three digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+Console.WriteLine(numberS.Length == 3 ? $"{number} -> {numberS[1]}" : "Not a three digit number entered");
+
+/* if(numberS.Length != 3) {
+    Console.WriteLine("You have entered not a three digit number");
+} else {
+    Console.WriteLine($"{number} -> {numberS[1]}");
+}
+*/
