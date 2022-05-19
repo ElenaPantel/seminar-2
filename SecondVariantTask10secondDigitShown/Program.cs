4525172e59e7c2d8de3b9b7362d3060b9642bc10
«Elena <lingelena@hotmail.com>
2022-05-19 23:15:05 +0200

    Third variant of task 10 Second digit shown of three digit number

--- SecondVariantTask10secondDigitShown/Program.cs
+++ SecondVariantTask10secondDigitShown/Program.cs
@@ -14,3 +14,22 @@ Console.WriteLine(numberS.Length == 3 ? $"{number} -> {numberS[1]}" : "Not a thr
     Console.WriteLine($"{number} -> {numberS[1]}");
 }
 */
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 999)
+{
+    Console.WriteLine("Я зря сказал трёхзначное?");
+}
+else if (num < 100)
+{
+    Console.WriteLine("Так, тут не хватает цифр");
+}
+else
+{
+    int mult1 = num % 100;
+    int mult2 = mult1 / 10;
+    Console.WriteLine($"Вторая цифра введённого числа {mult2}");
+}
+Console.ReadLine();
