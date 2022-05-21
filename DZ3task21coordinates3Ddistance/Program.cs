8063f0b3530af4014a9be26bab1e640adb3b7f47
«Elena <lingelena@hotmail.com>
2022-05-22 00:09:14 +0200

    DZ3 task 21 coordinates 3D distance between 2 points

--- DZ3task21coordinates3Ddistance/Program.cs
+++ DZ3task21coordinates3Ddistance/Program.cs
@@ -4,21 +4,21 @@
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Write("Enter the x coordinate of the point A: ");
-double xA=double.Parse(Console.ReadLine());
+double xA = double.Parse(Console.ReadLine());
 Console.Write("Enter the y coordinate of the point A: ");
-double yA=double.Parse(Console.ReadLine());
+double yA = double.Parse(Console.ReadLine());
 Console.Write("Enter the z coordinate of the point A: ");
-double zA=double.Parse(Console.ReadLine());
+double zA = double.Parse(Console.ReadLine());
 Console.Write("Enter the x coordinate of the point B: ");
-double xB=double.Parse(Console.ReadLine());
+double xB = double.Parse(Console.ReadLine());
 Console.Write("Enter the y coordinate of the point B: ");
-double yB=double.Parse(Console.ReadLine());
+double yB = double.Parse(Console.ReadLine());
 Console.Write("Enter the z coordinate of the point B: ");
-double zB=double.Parse(Console.ReadLine());
+double zB = double.Parse(Console.ReadLine());
 
 float getDistance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-   float result =(float)(Math.Sqrt((Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2))));
-   return result;
+    float result = (float)(Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2))));
+    return result;
 }
-Console.WriteLine("{0: 0.00}", getDistance(xA,yA,zA,xB,yB,zB));
+Console.WriteLine("{0: 0.00}", getDistance(xA, yA, zA, xB, yB, zB));
