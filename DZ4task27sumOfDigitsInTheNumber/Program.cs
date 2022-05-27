2ece6f1b0e93aaca29017cdbda0be95881fc2ce7
«Elena <lingelena@hotmail.com>
2022-05-27 22:56:23 +0200

    DZ 4 task 27 Sum of the digits in the number

---
+++ DZ4task27sumOfDigitsInTheNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе. Задачу решить без использования строк (string).
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Enter a number: ");
+int a = int.Parse(Console.ReadLine());
+int number = a;
+
+int sum = 0;
+
+while (a > 0)
+{
+    sum += a % 10;
+    a /= 10;
+}
+
+Console.WriteLine($"The sum of the digits in the number {number} is {sum}");
