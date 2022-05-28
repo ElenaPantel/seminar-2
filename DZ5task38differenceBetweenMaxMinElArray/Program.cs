d20d39728a974fdd2b137b4672e3345319a0376c
«Elena <lingelena@hotmail.com>
2022-05-28 23:38:15 +0200

    DZ 5 task 38 the Difference between Max and Min elements of Random Array

---
+++ DZ5task38differenceBetweenMaxMinElArray/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Random rnd = new Random();
+double[] nums = new double[10];
+int n = nums.Length;
+double min;
+double max;
+double diff;
+
+for (int i = 0; i < n; i++)
+{
+    nums[i] = rnd.Next(0, 100);
+}
+
+min = nums[0];
+max = nums[0];
+for (int i = 0; i < n; i++)
+{
+    if (nums[i] > max)
+    {
+        max = nums[i];
+    }
+    if (nums[i] < min)
+    {
+        min = nums[i];
+    }
+}
+
+diff = max - min;
+PrintArray(nums);
+Console.WriteLine($"-> Difference between max element {max} and min element {min} of the array is: {diff}");
+
+void PrintArray(double[] arr)
+{
+    string str = "[";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        str += $", {arr[i]}";
+    }
+
+    str = str.Remove(1, 2);
+    // str += "]";
+    Console.WriteLine($"Array: {str}]");
+}
